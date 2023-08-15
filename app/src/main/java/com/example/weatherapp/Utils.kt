615a08543b354eb3493812d0f46c2bf00f26aefb
DivaPutraRanasyah <hotarutar@gmail.com>
2023-08-16 00:09:50 +0700

    first commit

---
+++ app/src/main/java/com/example/weatherapp/Utils.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapp
+
+class Utils {
+    companion object{
+
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = "3e93b598490ac327f872b12f0853b399"
+         const val PERMISSION_REQUEST_CODE = 123
+
+
+
+
+
+
+    }
+}
